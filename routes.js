2f382659466ba49904873945cb30a972d2152e7b
Nick-Hericks <nickhericks@gmail.com>
2019-06-08 17:52:43 +0200

    Add auth middleware comments

--- routes.js
+++ routes.js
@@ -12,6 +12,37 @@ const router = express.Router();
 const users = [];
 
 
+const authenticateUser = (req, res, next) => {
+  // Parse the user's credentials from the Authorization header.
+
+  // If the user's credentials are available...
+  // Attempt to retrieve the user from the data store
+  // by their username (i.e. the user's "key"
+  // from the Authorization header).
+
+  // If a user was successfully retrieved from the data store...
+  // Use the bcryptjs npm package to compare the user's password
+  // (from the Authorization header) to the user's password
+  // that was retrieved from the data store.
+
+  // If the passwords match...
+  // Then store the retrieved user object on the request object
+  // so any middleware functions that follow this middleware function
+  // will have access to the user's information.
+
+  // If user authentication failed...
+  // Return a response with a 401 Unauthorized HTTP status code.
+
+  // Or if user authentication succeeded...
+  // Call the next() method.  next();
+};
+
+
+router.get("/users", authenticateUser, (req, res) => {
+  // Code to get and return the current user...
+});
+
+
 // Route that creates a new user.
 router.post('/users', [
   check('name')
